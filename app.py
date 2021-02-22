b23c6b3f34ea0b52fc2b56ad08aad0a32a3d8893
alphishahrin <62606857+alphishahrin@users.noreply.github.com>
2021-02-23 02:23:23 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,91 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'there is an issue'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html',tasks=tasks)
+    
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'there is an issue updating'
+            
+    else:
+        return render_template('update.html', task=task)
+
+@app.route('/calculator')
+def calculate():
+    return render_template('app.html')
+
+@app.route('/send', methods=['POST'])
+def send(sum=sum):
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        operation = request.form['operation']
+
+        if operation == 'add':
+            sum = float(num1) + float(num2)
+            return render_template('app.html', sum=sum)
+
+        elif operation == 'subtract':
+            sum = float(num1) - float(num2)
+            return render_template('app.html', sum=sum)
+
+        elif operation == 'multiply':
+            sum = float(num1) * float(num2)
+            return render_template('app.html', sum=sum)
+
+        elif operation == 'divide':
+            sum = float(num1) / float(num2)
+            return render_template('app.html', sum=sum)
+        else:
+            return render_template('app.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
